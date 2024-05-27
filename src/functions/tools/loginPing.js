161c6d979d23ca2da31cd7a57f6cbb261e64255d
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2024-05-27 03:03:55 -0400

    v1.7.1
    - I expanded the loginPing.js script to output login messages to telegram as a backup measure.
    
    - I also made the client.on disconnect and error use async like sigint to ensure the messages are delivered before closing the bot process. The previous version wouldn't send the messages since the process would be terminated immediately.
    
    - I also added axios and updated the dependencies to their latest versions.

--- src/functions/tools/loginPing.js
+++ src/functions/tools/loginPing.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const axios = require('axios');
 require('dotenv').config();
 
 module.exports = async (client) => {
@@ -31,11 +32,28 @@ module.exports = async (client) => {
         });
     }
 
+    const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
+    const CHAT_ID = process.env.TELEGRAM_CHAT_ID; // Your chat ID
+
+    function sendTelegramMessage(message) {
+        return axios.post(`${TELEGRAM_API_URL}/sendMessage`, {
+            chat_id: CHAT_ID,
+            text: message
+        })
+        .then(() => {
+            console.log('Telegram message sent successfully!');
+        })
+        .catch(error => {
+            console.error('Error sending Telegram message:', error);
+        });
+    }
+
     const executeLoginPing = async () => {
         // Check if client.user is available
         if (client.user) {
-            // Call sendEmail function to execute
-            await sendEmail(`${client.user.tag} Login`, `${client.user.tag} has successfully logged in.`);
+            const loginMessage = `${client.user.tag} has successfully logged in.`;
+            await sendEmail(`${client.user.tag} Login`, loginMessage);
+            sendTelegramMessage(loginMessage);
         } else {
             console.log('Bot has not logged in yet.');
         }
@@ -47,20 +65,25 @@ module.exports = async (client) => {
     });
 
     // Listen for the 'disconnect' event to notify when the bot goes offline
-    client.on('disconnect', () => {
-        sendEmail(`${client.user.tag} Offline`, `${client.user.tag} has gone offline.`);
+    client.on('disconnect', async () => {
+        const offlineMessage = `${client.user.tag} has gone offline.`;
+        await sendEmail(`${client.user.tag} Offline`, offlineMessage);
+        await sendTelegramMessage(offlineMessage);
     });
 
     // Listen for the 'error' event to notify when an error occurs
-    client.on('error', (error) => {
-        sendEmail(`${client.user.tag} Error`, `An error occurred: ${error.message}`);
+    client.on('error', async (error) => {
+        const errorMessage = `An error occurred: ${error.message}`;
+        await sendEmail(`${client.user.tag} Error`, errorMessage);
+        await sendTelegramMessage(errorMessage);
     });
 
     // Listen for the SIGINT signal to simulate disconnect
     process.on('SIGINT', async () => {
         console.log('SIGINT received. Terminating the bot...');
-        // Send email notification when terminating the bot
-        await sendEmail(`${client.user.tag} Disconnected`, `${client.user.tag} has been disconnected.`);
+        const disconnectMessage = `${client.user.tag} has been disconnected.`;
+        await sendEmail(`${client.user.tag} Disconnected`, disconnectMessage);
+        await sendTelegramMessage(disconnectMessage);
         process.exit(0); // Terminate the process
     });
 };
